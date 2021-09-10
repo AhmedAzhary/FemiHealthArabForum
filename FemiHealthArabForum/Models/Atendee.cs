ee111a904fe7fd615829741b2ef019fbc2926c41
AhmedAzhary <38773332+AhmedAzhary@users.noreply.github.com>
2021-09-10 04:06:38 +0200

    1- merge Json 2- listings

--- FemiHealthArabForum/Models/Atendee.cs
+++ FemiHealthArabForum/Models/Atendee.cs
@@ -7,7 +7,7 @@ namespace FemiHealthArabForum.Models
 {
     public class Atendee
     {
-        public int ID { get; set;  }        
+        public string ID { get; set;  }        
         public string Name { get;  set; }
         public string Email { get; set; }
         public string Phone { get;  set; }
